5e03d1808e2c7e0740c1d0f62481c2a16c34cd49
SEG-Austria <leckermegges@gmail.com>
2026-02-13 19:42:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,72 @@
-function login() {
-  alert("Login kommt gleich 😎");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxwD04ZcxDjKkzCjIGXtGOJsewkAdNg50",
+  authDomain: "seg-austria.firebaseapp.com",
+  projectId: "seg-austria",
+  storageBucket: "seg-austria.firebasestorage.app",
+  messagingSenderId: "101261189931",
+  appId: "1:101261189931:web:4f6b5bd9008f5f64bd1b6e",
+  measurementId: "G-C0QLYYD6Q5"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+function fakeEmail(username) {
+  return username + "@seg.local";
 }
+
+// 🟢 REGISTRIEREN
+window.register = async () => {
+  const u = username.value;
+  const p = password.value;
+
+  try {
+    const cred = await createUserWithEmailAndPassword(
+      auth,
+      fakeEmail(u),
+      p
+    );
+
+    await setDoc(doc(db, "users", cred.user.uid), {
+      username: u,
+      role: "Rekrut"
+    });
+
+    status.innerText = "Registriert ✔ Rolle: Rekrut";
+  } catch (e) {
+    status.innerText = e.message;
+  }
+};
+
+// 🔵 LOGIN
+window.login = async () => {
+  const u = username.value;
+  const p = password.value;
+
+  try {
+    const cred = await signInWithEmailAndPassword(
+      auth,
+      fakeEmail(u),
+      p
+    );
+
+    const snap = await getDoc(doc(db, "users", cred.user.uid));
+    status.innerText =
+      "Login ✔ Rolle: " + snap.data().role;
+  } catch (e) {
+    status.innerText = "Login fehlgeschlagen ❌";
+  }
+};
